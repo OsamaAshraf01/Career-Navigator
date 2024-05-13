390d1a841e6d09289ad813fedb9bd060be702b46
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-05-13 09:19:04 +0300

    Add: assets directoy

---
+++ src/assets/script.js
@@ -0,0 +1,28 @@
+var checks_count = 0;
+
+function append_check(label_text) {
+    checks_count += 1;
+    // Creating the box
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.name = "check_{checks_count}";
+    check.value = false;
+
+    // Creating the label
+
+    const label = document.createElement("label");
+    label.for = check.name
+    label.innerText = label_text;
+
+
+    document.getElementById('check-boxes-div').appendChild(check);
+    document.getElementById('check-boxes-div').appendChild(label);
+
+
+};
+
+
+
+function button_load(){
+    // To make loading button
+}
